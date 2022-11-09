0c0e2ca582425bb73acf64a260972df89207913d
anwardarkazanli <anwar905@revature.net>
2022-11-09 11:36:25 -0800

    Work on interests feature step 1.

---
+++ src/test/java/com/revature/stepimplementations/profile/profSteps.java
@@ -0,0 +1,7 @@
+package com.revature.stepimplementations.profile;
+
+public class profSteps {
+    @io.cucumber.java.en.Given("^The user is on the home page\\.$")
+    public void theUserIsOnTheHomePage() {
+    }
+}
