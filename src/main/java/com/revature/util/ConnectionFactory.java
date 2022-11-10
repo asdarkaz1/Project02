edf0ae4ad260c08dbe4182016f01678eb7fc8628
unknown <estimebrithnie@yahoo.com>
2022-11-09 22:59:05 -0500

    ...

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -7,19 +7,18 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-
-
     public static Connection createConnection() throws SQLException {
+        //Database credentials should not be hard coded use system environment
+        //variables. 20:53 (restart application after)
+        String url = System.getenv("");
+        String username = System.getenv("");
+        String password = System.getenv("");
 
-        //Database credentials should not be hard coded use system environment variables. 20:53 (restart application after)
-   String url = System.getenv("");
-   String username = System.getenv("");
-   String password = System.getenv("");
+        //Instantiating connection object
+        Connection connection = DriverManager.getConnection(url, username, password);
 
-    //Instantiating connection object
-    Connection connection = DriverManager.getConnection(url, username, password);
-
-    return connection;
+        return connection;
 
     }
+
 }
