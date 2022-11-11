70f103911b354682a25dbbc9e411511cbba284bb
anwardarkazanli <anwar905@revature.net>
2022-11-11 05:45:45 -0800

    changed the action attribute on html form and began the api backend

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -8,6 +8,7 @@ import com.revature.dao.PostDao;
 import com.revature.model.Comment;
 import com.revature.model.Post;
 import io.javalin.Javalin;
+import org.eclipse.jetty.server.Authentication;
 import org.postgresql.Driver;
 
 import java.sql.Connection;
@@ -20,20 +21,55 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
+        //create Javalin object and return it
         Javalin app = Javalin.create();
 
-        //actually mapping the endpoints for comment controller
 
-        //actually mapping the endpoints for post controller
+        //Endpoint1 (Delete this endpoint);
+        app.get("/profile", (ctx) -> {
+            //1.Write the JDBC that will go and query the database for all users.
+            String url = "jdbc:postgresql://34.134.127.27:5432/postgres";
+            String username = "postgres";
+            String password = "revstarProject02";
+            Driver postgresDriver = new Driver();
+            DriverManager.registerDriver(postgresDriver);
+            Connection connection = DriverManager.getConnection(url,username, password);
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Profile");
+            ResultSet rs = pstmt.executeQuery();
+            List<Profile> allProfiles = new ArrayList<>();
+        });
 
-        //Controller is an interface that is implemented by these controllers.
-        Controller[] controllers ={
-                new CommentController(), new PostController()
-        };
 
-        for(Controller c : controllers){
-            c.mapEndPoints(app);
-        }
+//Endpoint 2
+app.post("/profile",(ctx) -> {
+    System.out.println("Connected");
+    String user_name = ctx.pathParam("username2");
+    String email;
+    String pass_word;
+    String interest;
+    String firstname;
+    String lastname;
+    String url = "jdbc:postgresql://34.134.127.27:5432/postgres";
+    String username = "postgres";
+    String password = "revstarProject02";
+    Driver postgresDriver = new Driver();
+    DriverManager.registerDriver(postgresDriver);
+    Connection connection = DriverManager.getConnection(url,username, password);
+    //Passing information to the query  //4:35
+    PreparedStatement pstmt = connection.prepareStatement("INSERT INTO " +
+            "users (username, email, password, interest, firstname, lastname) " +
+            "VALUES (? ,?, ?, ?, ?, ?)");
+    pstmt.setString(1,user_name);
+
+    pstmt.executeQuery();
+
+    //Take JSON information and create a profile object.
+
+
+
+
+});
         app.start(8080);
     }
 
